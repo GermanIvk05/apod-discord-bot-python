52b5013128bb1fd51691330d687516776a415b10
GermanIvk05 <48442292+GermanIvk05@users.noreply.github.com>
2024-01-14 02:33:11 -0500

    Added: advanced discord bot startup

--- src/bot.py
+++ src/bot.py
@@ -1,3 +1,10 @@
+"""
+Based on one of the examples from https://github.com/Rapptz/discord.py
+"""
+
+import asyncio
+import logging
+import logging.handlers
 import os
 
 import discord
@@ -12,17 +19,46 @@ description = '''
 Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.
 '''
 
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+class SpaceBot(commands.Bot):
+
+    def __init__(self, *args, initial_extensions: list[str], **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.initial_extensions = initial_extensions
+
+    async def setup_hook(self) -> None:
+        for extension in self.initial_extensions:
+            await self.load_extension(extension)
+
+        # TODO: implement a check to sync only when the changes are made
+        synced = await self.tree.sync()
+        print(f"Synced {len(synced)} command(s).")
 
 
-@bot.event
-async def on_ready() -> None:
-    print(f" Logged on as {bot.user}!")
-    await bot.load_extension("spacestop")
+async def main() -> None:
+    logger = logging.getLogger("discord")
+    logger.setLevel(logging.INFO)
 
-    synced = await bot.tree.sync()
-    print(f"Synced {len(synced)} command(s).")
+    handler = logging.handlers.RotatingFileHandler(
+        filename="discord.log",
+        encoding="utf-8",
+        maxBytes=32 * 1024 * 1024,
+        backupCount=5
+    )
+    dt_fmt = "%Y-%m-%d %H:%M:%S"
+    formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    extensions = ["spacestop"]
+    intents = discord.Intents.default()
+    async with SpaceBot(
+        commands.when_mentioned,
+        initial_extensions=extensions, 
+        description=description,
+        intents=intents
+    ) as bot:
+        await bot.start(TOKEN)
+
 
 if __name__ == "__main__":
-    bot.run(TOKEN)
+    asyncio.run(main())
